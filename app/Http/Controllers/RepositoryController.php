c5a50ea8bca1c0b287a7a40fdbf30490f4bb541c
jose.regalado <jose.regalado@irapuato.gob.mx>
2023-01-18 15:10:34 -0600

    Más pruebas y validaciones

--- app/Http/Controllers/RepositoryController.php
+++ app/Http/Controllers/RepositoryController.php
@@ -2,15 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Repository;
 use Illuminate\Http\Request;
 
 class RepositoryController extends Controller
 {
+
+    public function index(Request $request)
+    {
+        return view('repositories.index', [
+            'repositories' => $request->user()->repositories
+        ]);
+    }
+
+    public function show(Request $request, Repository $repository)
+    {
+        if ($request->user()->id != $repository->user_id) {
+            abort(403);
+        }
+        return view('repositories.show', compact('repository'));
+    }
+
     //
     public function store(Request $request)
     {
+        $request->validate([
+            'url' => 'required',
+            'description' => 'required',
+        ]);
+
         $request->user()->repositories()->create($request->all());
 
+        return redirect()->route('repositories.index');
+    }
+
+
+    public function update(Request $request, Repository $repository)
+    {
+        $request->validate([
+            'url' => 'required',
+            'description' => 'required',
+        ]);
+
+        if ($request->user()->id != $repository->user_id) {
+            abort(403);
+        }
+
+        $repository->update($request->all());
+        return redirect()->route('repositories.edit', $repository);
+    }
+
+    public function destroy(Request $request, Repository $repository)
+    {
+        if ($request->user()->id != $repository->user_id) {
+            abort(403);
+        }
+
+        $repository->delete();
         return redirect()->route('repositories.index');
     }
 }
--- tests/Feature/Controllers/RepositoryControllerTest.php
+++ tests/Feature/Controllers/RepositoryControllerTest.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
-
+use  App\Models\Repository;
 
 class RepositoryControllerTest extends TestCase
 {
@@ -29,9 +29,34 @@ class RepositoryControllerTest extends TestCase
         $this->post('repositories', [])->assertRedirect('login');    //store
     }
 
-    public function test_store()
+
+    public function test_index_empty()
     {
 
+        $user = User::factory()->create();
+        Repository::factory()->create();
+
+        $this->actingAs($user)
+            ->get('repositories')
+            ->assertStatus(200)
+            ->assertSee('No hay repositorios creados');
+    }
+
+    public function test_index_with_data()
+    {
+
+        $user = User::factory()->create();
+        $repository = Repository::factory()->create(['user_id' => $user->id]);
+
+        $this->actingAs($user)
+            ->get('repositories')
+            ->assertStatus(200)
+            ->assertSee($repository->id)
+            ->assertSee($repository->url);
+    }
+
+    public function test_store()
+    {
         $user = User::factory()->create();
 
         $data = [
@@ -44,5 +69,122 @@ class RepositoryControllerTest extends TestCase
             ->assertRedirect('repositories');
 
         $this->assertDatabaseHas('repositories', $data);
+    }
+
+    public function test_update()
+    {
+
+        $user = User::factory()->create();
+        $repository = Repository::factory()->create(['user_id' => $user->id]);
+
+        $data = [
+            'url'   => $this->faker->url,
+            'description' => $this->faker->text,
+        ];
+
+        $this->actingAs($user)
+            ->put("repositories/$repository->id", $data)
+            ->assertRedirect("repositories/$repository->id/edit");
+
+        $this->assertDatabaseHas('repositories', $data);
+    }
+
+
+    public function test_update_policy()
+    {
+
+        $user = User::factory()->create();
+        $repository = Repository::factory()->create();
+
+        $data = [
+            'url'   => $this->faker->url,
+            'description' => $this->faker->text,
+        ];
+
+        $this->actingAs($user)
+            ->put("repositories/$repository->id", $data)
+            ->assertStatus(403);
+    }
+
+    public function test_destroy()
+    {
+
+        $user = User::factory()->create();
+        $repository = Repository::factory()->create(['user_id' => $user->id]);
+
+        $this->actingAs($user)
+            ->delete("repositories/$repository->id")
+            ->assertRedirect("repositories");
+
+        $this->assertDatabaseMissing(
+            'repositories',
+            [
+                'id' => $repository->id,
+                'url' => $repository->url,
+                'description' => $repository->description,
+            ]
+        );
+    }
+
+
+    public function test_destroy_policy()
+    {
+        $user = User::factory()->create();
+        $repository = Repository::factory()->create();
+
+        $this->actingAs($user)
+            ->delete("repositories/$repository->id")
+            ->assertStatus(403);
+    }
+
+    public function test_validate_store()
+    {
+
+        $user = User::factory()->create();
+
+
+        $this->actingAs($user)
+            ->post('repositories', [])
+            ->assertStatus(302)
+            ->assertSessionHasErrors(['url', 'description']);
+    }
+
+    public function test_validate_update()
+    {
+
+        $user = User::factory()->create();
+        $repository = Repository::factory()->create();
+
+
+        $this->actingAs($user)
+            ->put("repositories/$repository->id", [])
+            ->assertStatus(302)
+            ->assertSessionHasErrors(['url', 'description']);
+    }
+
+
+
+
+    public function test_show()
+    {
+
+        $user = User::factory()->create();
+        $repository = Repository::factory()->create(['user_id' => $user->id]);
+
+        $this->actingAs($user)
+            ->get("repositories/$repository->id")
+            ->assertStatus(200);
+    }
+
+
+    public function test_show_policy()
+    {
+
+        $user = User::factory()->create();
+        $repository = Repository::factory()->create();
+
+        $this->actingAs($user)
+            ->get("repositories/$repository->id")
+            ->assertStatus(403);
     }
 }
